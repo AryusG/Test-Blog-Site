a2fe54f7a25a4bab310625f7af79f805f017c98f
AryusG <verdyg24@gmail.com>
2022-01-06 21:26:44 +0000

    Finished

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from post import Post
 
 app = Flask(__name__)
 
+p = Post("https://api.npoint.io/c790b4d5cab58020d391")
+json = p.json
+
 @app.route('/')
 def home():
-    p = Post("https://api.npoint.io/c790b4d5cab58020d391")
-    # iterate through len of p.json to create cards for main page 
-    return render_template("index.html")
+    return render_template("index.html", json=json)
+
+@app.route('/post/<int:index>')
+def post(index):
+    return render_template("post.html", json=json, index=index)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- post.py
+++ post.py
@@ -13,7 +13,10 @@ class Post:
         return json
 
 
-# p = Post("https://api.npoint.io/c790b4d5cab58020d391")
+p = Post("https://api.npoint.io/c790b4d5cab58020d391")
 
-# print(p.json[0])
-# # print(p.get_title())
+# for element in p.json:
+
+#     print(element)
+# print(p.get_title())
+print(len(p.json))
